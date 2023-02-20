7d687891c4bd502b458f0d0c161c68cca77b2538
Chirag <chiragpatel140601@gmail.com>
2023-02-20 12:21:21 +0530

    corrected modul.lib

--- alb/variables.tf
+++ alb/variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  type = string
-  description = "The AWS region in which to launch the resources."
-}
-
 variable "name" {
   type = string
   description = "The AWS region in which to launch the resources."
