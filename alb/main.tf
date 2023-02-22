8fb241fb9bd1dd34aedd12be1588c0efc985cc00
Chirag <chiragpatel140601@gmail.com>
2023-02-22 16:25:16 +0530

    1st time ecs

--- alb/main.tf
+++ alb/main.tf
@@ -67,9 +67,8 @@ resource  "aws_lb_target_group" "alb_target_group" {
   }
 }
 
-/*resource "aws_lb_target_group_attachment" "alb_tg_attachment" {
+resource "aws_lb_target_group_attachment" "alb_tg_attachment" {
   target_group_arn = aws_lb_target_group.alb_target_group.arn
-  target_id        = aws_instance.web.id
   port             = 80
-}*/
+}
 
