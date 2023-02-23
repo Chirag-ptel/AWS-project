0b76545a4a8d15dcd7903b6338152e5deb5a12c7
Chirag <chiragpatel140601@gmail.com>
2023-02-23 15:02:07 +0530

    1st time ecs

--- vpc/main.tf
+++ vpc/main.tf
@@ -132,3 +132,46 @@ resource "aws_route_table" "private" {
   }
 }
 
+resource "aws_security_group" "public" {
+  name_prefix = "example-public-sg-"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private" {
+ name_prefix = "example-private-sg-"
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = aws_subnet.public[*].cidr_block
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
