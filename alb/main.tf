c56fb1b62f9926e8536cbca2a775a46836c9db40
Chirag <chiragpatel140601@gmail.com>
2023-02-24 09:20:07 +0530

    1st time ecs

--- alb/main.tf
+++ alb/main.tf
@@ -27,7 +27,7 @@ resource "aws_lb" "alb" {
   name               = var.name
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [module.lib.default_security_group_id]
+  security_groups    = [aws_lb_security_group.lb_security_group.id]
   subnets            = module.lib.public_subnets
 
   tags = {
@@ -44,7 +44,7 @@ resource "aws_lb_listener" "alb_listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
-}
+} 
 
 resource  "aws_lb_target_group" "alb_target_group" {
   name               = var.name
@@ -69,15 +69,31 @@ resource  "aws_lb_target_group" "alb_target_group" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "alb_tg_attachment" {
+/*resource "aws_lb_target_group_attachment" "alb_tg_attachment" {
   target_group_arn = aws_lb_target_group.alb_target_group.arn
   target_id        = module.lib.ecs-service-id
   port             = 80
 
-  /*depends_on = [
+  depends_on = [
     aws_lb_listener.alb_listener,
     aws_ecs_service.ecs-service,
-  ]*/
+  ]
+}*/
+
+resource "aws_security_group" "lb_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
