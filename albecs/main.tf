a4a188532e0b5c9e30fdb0f29c9d7df552365b3a
Chirag <chiragpatel140601@gmail.com>
2023-02-27 11:07:23 +0530

    added 443 listener

--- albecs/main.tf
+++ albecs/main.tf
@@ -136,6 +136,13 @@ resource "aws_security_group" "lb_security_group" {
     cidr_blocks = ["0.0.0.0/0"] 
   }
 
+ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
