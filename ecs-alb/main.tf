3eba2d958306c6f758b316167d739e445bdbc1a7
Chirag <chiragpatel140601@gmail.com>
2023-03-12 22:31:21 +0530

    finalised ecs

--- ecs-alb/main.tf
+++ ecs-alb/main.tf
@@ -191,7 +191,7 @@ resource "aws_alb_listener" "alb_listener_443" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "443"
   protocol          = "HTTPS"
-  /*certificate_arn = "arn:aws:acm:us-east-1:155358046204:certificate/5d08eb6f-0065-4ec5-a51f-5ee2ddb78eb5"*/
+  certificate_arn = "arn:aws:acm:us-east-1:155358046204:certificate/5d08eb6f-0065-4ec5-a51f-5ee2ddb78eb5"
    
   default_action {
     target_group_arn = aws_lb_target_group.alb_target_group.arn
