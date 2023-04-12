9e43b44bee603c95192f483624dfd6f3d9b21678
Chirag <chiragpatel140601@gmail.com>
2023-04-12 17:25:42 +0530

    RP Image

--- ecs-alb/main.tf
+++ ecs-alb/main.tf
@@ -167,12 +167,12 @@ resource "aws_lb_listener" "alb_listener" {
   protocol          = "HTTP"
 
   default_action {
-    type             = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301" 
-    }
+    type             = "forward"
+    # redirect {
+    #   port        = "443"
+    #   protocol    = "HTTPS"
+    #   status_code = "HTTP_301" 
+    # }
     target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
 }
