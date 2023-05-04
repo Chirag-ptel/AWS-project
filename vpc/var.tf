57faa37775ab38d6e9aaf733dc4e8ccda52be950
Chirag <chiragpatel140601@gmail.com>
2023-05-04 16:00:58 +0530

    ........

--- vpc/var.tf
+++ vpc/var.tf
@@ -6,5 +6,5 @@ variable "region" {
 variable "availability_zones" {
   description = "The availability zones for the subnets"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
 }
